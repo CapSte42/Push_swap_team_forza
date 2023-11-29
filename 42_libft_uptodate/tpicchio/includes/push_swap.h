f4da769d68431ad773816145d616db7251d7506b
tpicchio <tpicchio@student.42firenze.it>
2023-11-29 17:00:18 +0100

    animazione funzionante

--- 42_libft_uptodate/tpicchio/includes/push_swap.h
+++ 42_libft_uptodate/tpicchio/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tpicchio <tpicchio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:23:50 by tpicchio          #+#    #+#             */
-/*   Updated: 2023/11/29 14:19:05 by tpicchio         ###   ########.fr       */
+/*   Updated: 2023/11/29 15:56:10 by tpicchio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ t_list	*ft_obm_score(t_list *stack_b);
 size_t	ft_max_index(t_list *stack_a);
 
 void	ft_print_status(t_list *stack_a, t_list *stack_b, char *moves);
+void ft_animation(t_list *stack_a, t_list *stack_b, char *moves);
 
 #endif
--- 42_libft_uptodate/tpicchio/print_status.c
+++ 42_libft_uptodate/tpicchio/print_status.c
@@ -6,7 +6,7 @@
 /*   By: tpicchio <tpicchio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 14:12:03 by tpicchio          #+#    #+#             */
-/*   Updated: 2023/11/28 14:41:27 by tpicchio         ###   ########.fr       */
+/*   Updated: 2023/11/29 16:58:04 by tpicchio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_print_status(t_list *stack_a, t_list *stack_b, char *moves)
 {
-	//printf("\033[2J\033[H");
+	printf("\033[2J\033[H");
 	printf("\tStack A\t\tStack B\n");
 	while (stack_a || stack_b != NULL)
 	{
@@ -33,6 +33,78 @@ void	ft_print_status(t_list *stack_a, t_list *stack_b, char *moves)
 		else
 			printf("\n");
 	}
-	printf("Moves:\n%s", moves);
+	printf("Moves:%s\n", moves);
 	sleep(1);
+}
+
+void ft_animation(t_list *stack_a, t_list *stack_b, char *moves)
+{
+	char	**split;
+	int		i;
+
+	split = ft_split(moves, '\n');
+	i = 0;
+	while (split[i])
+	{
+		if (split[i][0] == 's' && split[i][1] == 'a' && split[i][2] == '\0')
+		{
+			ft_sx(&stack_a);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 's' && split[i][1] == 'b' && split[i][2] == '\0')
+		{
+			ft_sx(&stack_b);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 's' && split[i][1] == 's' && split[i][2] == '\0')
+		{
+			ft_sx(&stack_a);
+			ft_sx(&stack_b);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 'p' && split[i][1] == 'a' && split[i][2] == '\0')
+		{
+			ft_px(&stack_a, &stack_b);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 'p' && split[i][1] == 'b' && split[i][2] == '\0')
+		{
+			ft_px(&stack_b, &stack_a);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 'r' && split[i][1] == 'a' && split[i][2] == '\0')
+		{
+			ft_rx(&stack_a);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 'r' && split[i][1] == 'b' && split[i][2] == '\0')
+		{
+			ft_rx(&stack_b);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 'r' && split[i][1] == 'r' && split[i][2] == '\0')
+		{
+			ft_rx(&stack_a);
+			ft_rx(&stack_b);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 'r' && split[i][1] == 'r' && split[i][2] == 'a')
+		{
+			ft_rrx(&stack_a);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 'r' && split[i][1] == 'r' && split[i][2] == 'b')
+		{
+			ft_rrx(&stack_b);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		else if (split[i][0] == 'r' && split[i][1] == 'r' && split[i][2] == 'r')
+		{
+			ft_rrx(&stack_a);
+			ft_rrx(&stack_b);
+			ft_print_status(stack_a, stack_b, split[i]);
+		}
+		i++;
+	}
+	//ft_print_status(stack_a, stack_b, "");
 }
